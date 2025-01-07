0ca93b7ab49ee2e73cbe7dbf9a20e011f29e4882
Cameron <vizgon@proton.me>
2025-01-07 13:39:31 -0700

    Fixed the names of everything.

--- src/main/java/stellar/organize/OrganizeController.java
+++ src/main/java/stellar/organize/OrganizeController.java
@@ -3,7 +3,7 @@ package stellar.organize;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class OrganizeController {
     @FXML
     private Label welcomeText;
 
